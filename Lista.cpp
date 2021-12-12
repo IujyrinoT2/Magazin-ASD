20d149bbb4bbdd7175db1143f84c29e07d23265f
Catalin.Iancu <IancuCatalinCristian@protonmail.com>
2021-12-12 11:46:24 +0200

    Rezolvare buguri pentru afisare meniu si pentru schimbare cantitate produs
    -> Programul este acum functional.
    -> Afisajul meniului functioneaza.
    -> Functia de Adaugare produs poate sa schimbe si cantitatea unui produs deja existent acum.

--- Lista.cpp
+++ Lista.cpp
@@ -53,13 +53,12 @@ void Lista::AdaugaProdus()
         p->SetCantitateProdus(cantitateProdusNou);
         if (this->_HEAD == nullptr)
         {
-            this->_HEAD = p;
+            this->_HEAD->SetNext(p);
             p->SetNext(nullptr);
             cout << endl << "Noul produs a fost adaugat cu succes! ";
             return;
         }
-        p->SetNext(this->_HEAD);
-        this->_HEAD = p;
+        this->_HEAD->SetNext(p);
         cout << endl << "Noul produs a fost adaugat cu succes! ";
         return;
     } else if (optiune == 2)
@@ -67,6 +66,19 @@ void Lista::AdaugaProdus()
         //aici actualizam stocul unui produs existen
         //utilizatorul trebuie sa vada lista cu produse deja existente / check sa nu fie goala
         //alege un cod - este verificat daca exista in lista si apoi se actualizeaza stocul
+        cout << "Acesta este stocul curent de produse:" << endl;
+        this->AfiseazaLista();
+        int codProdusIntrodus;
+        int cantitateNoua;
+        cout << "Introduceti codul produsului al carui cantitate doriti sa schimbati: ";
+        cin >> codProdusIntrodus;
+        cout << endl;
+        cout << "Introduceti valoarea noua pentru cantitatea produsului: ";
+        cin >> cantitateNoua;
+        cout << endl;
+        this->CautaProdus(codProdusIntrodus)->SetCantitateProdus(cantitateNoua);
+        cout << "Actualizarea cantitatii a avut succes." << endl;
+        this->AfiseazaProdus(CautaProdus(codProdusIntrodus));
         return;
     }
     cout << "Optiune invalida." << endl;
@@ -109,18 +121,89 @@ void Lista::AfiseazaProdus(Produs *produsCautat)
     }
     //Continutul liniei VV
     Table afisareElement;
-    char codProdus[3];
-    char numeProdus[20];
-    char cantitateProdus[20];
     char pretProdus[20];
-    itoa(produsCautat->GetCodProdus(), codProdus, 10);
-    strcpy(numeProdus, produsCautat->GetNumeProdus().c_str());
-    itoa(produsCautat->GetCantitateProdus(), cantitateProdus, 10);
     sprintf(pretProdus, "%.2f", produsCautat->GetPretProdus());
     afisareElement.add_row({"COD PRODUS", "DENUMIRE PRODUS", "CANTITATE", "PRET"});
-    afisareElement.add_row({codProdus, numeProdus, cantitateProdus, pretProdus});
+    afisareElement[0][0].format()
+            .corner_top_left("*")
+            .corner_top_right("*")
+            .corner_bottom_left("*")
+            .corner_bottom_right("*")
+            .corner_top_left_color(Color::red)
+            .corner_top_right_color(Color::red)
+            .corner_bottom_left_color(Color::red)
+            .corner_bottom_right_color(Color::red)
+            .font_align(FontAlign::center)
+            .font_color(Color::green)
+            .border_top("~")
+            .border_bottom("~")
+            .border_left("|")
+            .border_right("|")
+            .border_left_color(Color::cyan)
+            .border_right_color(Color::cyan)
+            .border_top_color(Color::cyan)
+            .border_bottom_color(Color::cyan);
+    afisareElement[0][1].format()
+            .corner_top_left("*")
+            .corner_top_right("*")
+            .corner_bottom_left("*")
+            .corner_bottom_right("*")
+            .corner_top_left_color(Color::red)
+            .corner_top_right_color(Color::red)
+            .corner_bottom_left_color(Color::red)
+            .corner_bottom_right_color(Color::red)
+            .font_align(FontAlign::center)
+            .font_color(Color::green)
+            .border_top("~")
+            .border_bottom("~")
+            .border_left("|")
+            .border_right("|")
+            .border_left_color(Color::cyan)
+            .border_right_color(Color::cyan)
+            .border_top_color(Color::cyan)
+            .border_bottom_color(Color::cyan);
+    afisareElement[0][2].format()
+            .corner_top_left("*")
+            .corner_top_right("*")
+            .corner_bottom_left("*")
+            .corner_bottom_right("*")
+            .corner_top_left_color(Color::red)
+            .corner_top_right_color(Color::red)
+            .corner_bottom_left_color(Color::red)
+            .corner_bottom_right_color(Color::red)
+            .font_align(FontAlign::center)
+            .font_color(Color::green)
+            .border_top("~")
+            .border_bottom("~")
+            .border_left("|")
+            .border_right("|")
+            .border_left_color(Color::cyan)
+            .border_right_color(Color::cyan)
+            .border_top_color(Color::cyan)
+            .border_bottom_color(Color::cyan);
+    afisareElement[0][3].format()
+            .corner_top_left("*")
+            .corner_top_right("*")
+            .corner_bottom_left("*")
+            .corner_bottom_right("*")
+            .corner_top_left_color(Color::red)
+            .corner_top_right_color(Color::red)
+            .corner_bottom_left_color(Color::red)
+            .corner_bottom_right_color(Color::red)
+            .font_align(FontAlign::center)
+            .font_color(Color::green)
+            .border_top("~")
+            .border_bottom("~")
+            .border_left("|")
+            .border_right("|")
+            .border_left_color(Color::cyan)
+            .border_right_color(Color::cyan)
+            .border_top_color(Color::cyan)
+            .border_bottom_color(Color::cyan);
 
-    afisareElement.format() //CULOOOOOOOOOOORI
+    afisareElement.add_row({to_string(produsCautat->GetCodProdus()), produsCautat->GetNumeProdus(),
+                            to_string(produsCautat->GetCantitateProdus()), pretProdus});
+    afisareElement.format()
             .corner_top_left("*")
             .corner_top_right("*")
             .corner_bottom_left("*")
@@ -194,7 +277,7 @@ Produs *Lista::CautaProdus(int codCautat)
 
 bool Lista::ValideazaStoc(int cantitateCumparata, Produs *produsCurent)
 {
-  return true;
+    return true;
 }
 
 void Lista::AfiseazaLista()
@@ -265,7 +348,7 @@ void Lista::AfiseazaLista()
             .border_right_color(Color::cyan)
             .border_top_color(Color::cyan)
             .border_bottom_color(Color::cyan);
-    Produs *nodCrt = this->_HEAD;
+    Produs *nodCrt = this->_HEAD->GetNext();
     int counter = 1;
     while (nodCrt != NULL)
     {
